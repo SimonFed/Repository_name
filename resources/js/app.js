e50044efe849a9da897675dd160af095840f467b
Your Name <you@example.com>
2023-06-28 17:20:48 +0300

    hhu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,32 @@
 import Navigation from './modules/navigation'
 
 Navigation.init();
+
+async function getData(offset, data = []) {
+    const response = await fetch('http://192.168.0.118:3000/items', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            offset: offset,
+        })
+    });
+
+    let items = await response.json();
+    if(items.length == 0) {
+        return data;
+    }
+
+    items.forEach(item => {
+        data.push(item);
+    });
+
+    return getData(offset + 2, data);
+}
+    getData(0).then((items) => {
+        console.log(items);
+
+    });
+
+
